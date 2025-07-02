0747feb1e8a388ff6360e851962298947bf87fd3
Ricardo Sousa <ricardosousa@Ricardos-MacBook-Air.local>
2025-07-02 14:07:34 +0100

    guardar

--- apps/jogos/models.py
+++ apps/jogos/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
+from apps.atletas.models import Atleta
 
+class Equipas(models.Model):
+    nome = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.nome
 
 
 class Jogos(models.Model):
-    nome = models.CharField(max_length=100)
-    data = models.DateField()
-    hora = models.TimeField()
-    local = models.CharField(max_length=100)
+    adversario = models.CharField(max_length=100)
+    data = models.DateTimeField(null=True,blank=True)
+    casa = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -17,10 +23,12 @@ class Jogos(models.Model):
 
 
 class EquipaJogo(models.Model):
+    atleta = models.ForeignKey(Atleta, on_delete=models.CASCADE)
     jogo = models.ForeignKey(Jogos, on_delete=models.CASCADE)
     capitao = models.BooleanField(default=False)
     titular = models.BooleanField(default=False)
-    suplente = models.BooleanField(default=False)
+    golos = models.IntegerField(default=0)
+    assistencias = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- apps/jogos/tests.py
+++ apps/jogos/tests.py
@@ -1,6 +1,10 @@
 import sys
 import os
+from django.test import Client, TestCase
 
+from apps.atletas.models import Atleta
+from apps.jogos.models import Equipas, EquipaJogo,Jogos
+from django.utils import timezone
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.insert(0, BASE_DIR)
 
@@ -9,3 +13,49 @@ import django
 django.setup()
 
 from django.test import TestCase
+
+class JogosTest(TestCase):
+    def _setUp(self):
+        self.cliente = Client()
+        self.atleta = Atleta.objects.create(
+            nome='Manuel Sousa',
+            data_nascimento='1999-01-01',
+            numero=13,
+            encarregado='Ricardo Sousa',
+            telefone='123456789',
+            email='YH6Q0@example.com',
+            guarda_redes=True
+        )
+        self.adversario= Equipas.objects.create(
+            nome='FCP'
+        )
+        self.jogo = EquipaJogo.objects.create(
+            adversario='jogo principal',
+            atleta=self.atleta
+        )
+
+    def test_criar_jogo(self):
+        jogo = EquipaJogo.objects.create(
+            adversario=self.adversario,
+            data = timezone.now(),
+            casa = True,
+
+        )
+        self.assertEqual(jogo.adversario, self.adversario)
+
+    def test_jodador_jogo(self):
+        jogador = EquipaJogo.objects.create(
+            atleta=self.atleta,
+            jogo = self.jogo,
+            titular = True,
+            capitao = True,
+            golos =1,
+            assistencias = 0
+
+        )
+        self.assertEqual(jogador.atleta, self.atleta)
+        self.assertEqual(jogador.jogo, self.jogo)
+        self.assertEqual(jogador.titular, True)
+        self.assertEqual(jogador.capitao, True)
+        self.assertEqual(jogador.golos, 1)
+        self.assertEqual(jogador.assistencias, 0)
