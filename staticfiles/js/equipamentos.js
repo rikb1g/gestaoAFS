ed394d263b3699af42f66d37269b1c2eb76f65b1
Rikb1g <r.sousa.888@gmail.com>
2025-08-14 17:03:22 +0100

    adicionar statics

---
+++ staticfiles/js/equipamentos.js
@@ -0,0 +1,165 @@
+function carregarConteudo(url) {
+    fetch(url, { headers: { "X-Requested-With": "XMLHttpRequest" } })
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("conteudo-dinamico").innerHTML = html;
+            history.pushState({ url: url }, "", url); // Atualiza a URL do navegador
+        })
+        .catch(error => console.error("Erro na requisição:", error));
+}
+
+var atletaID = sessionStorage.getItem("atletaID");
+if (atletaID) {
+    document.getElementById("atletasSelect").value = atletaID;
+    document.getElementById("atletasSelect").dispatchEvent(new Event("change"));
+}
+
+
+
+function atletaSelectEncomendas() {
+    console.log("atletaSelectEncomendas");
+    var atletaIDSession = document.getElementById("atletasSelect").value;
+    sessionStorage.setItem("atletaID", atletaIDSession);
+
+    var estadoEncomendaSession = document.getElementById("estadoEncomendaSelect").value
+    sessionStorage.setItem("estadoEncomenda", estadoEncomendaSession);
+    var estadoEncomenda = sessionStorage.getItem("estadoEncomenda");
+
+    var atletaID = sessionStorage.getItem("atletaID");
+    let bodyEncomendas = $('.tableEncomendas tbody');
+
+    if (atletaID == "todos") {
+        carregarConteudo(window.urlEncomendasList);
+
+    }
+
+    else if (atletaID.length > 0) {
+
+        $.ajax({
+            url: '/equipamentos/encomendas_por_atleta/' + atletaID + '/' + estadoEncomenda + '/',
+            method: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                let resultados = data.resultados
+
+
+                if (resultados.length > 0) {
+                    bodyEncomendas.empty()
+
+                    resultados.forEach(item => {
+                        let row = `<tr>
+                                    <td><input type="checkbox" name="selectEncomenda" class="form-check-input selectEncomenda" value="${item.id}">
+                                        </td>
+                                    <td>${item.atleta}</td>
+                                    <td>${item.equipamento}</td>
+                                    <td>${item.tamanho}</td>
+                                    <td>${item.data_pedido}</td> 
+                                    <td><input onchange="alterarEstadoEncomenda('${item.id}')" type="checkbox" name="entregue" id="entregue" ${item.entregue ? 'checked' : ''}></td>
+                                    <td class="linha">
+                                    <a href="/equipamentos/encomendas_uptade/${item.id}/"
+                                            hx-get="/equipamentos/encomendas_uptade/${item.id}/"
+                                            hx-target="#conteudo-dinamico"
+                                            hx-swap="outerHTML"
+                                        hx-push-url="true"
+                                        ><span
+                                    class="material-symbols-outlined btn-operacoes"> edit</span></a>
+                                    <a href="#" onclick="deleteEncomenda('${item.id}','${item.atleta}','${item.equipamento}')">
+                                    <span class="material-symbols-outlined btn-operacoes">delete</span></a>
+                                    </td>
+                                    </tr>`
+
+                        bodyEncomendas.append(row)
+                    })
+                } else {
+                    bodyEncomendas.empty()
+                    let row = `<tr>
+                              <td colspan="6">Nenhuma encomenda encontrada</td>
+                              </tr>`
+                    bodyEncomendas.append(row)
+                }
+            },
+            error: function (data) {
+                bodyEncomendas.html('<tr><td colspan="3">Erro ao buscar encomendas.</td></tr>');
+            }
+        })
+
+
+    }
+}
+
+$(document).on('submit', '#form-new-encomenda', function (event) {
+    event.preventDefault();
+    console.log("Form encomendas encontrado");
+
+    const formData = new FormData(this);
+    var url = $(this).attr('data-url');
+    console.log("URL:", url);
+
+    fetch(url, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Resposta do servidor:", data);
+            if (data.success) {
+                alert(data.message);
+                carregarConteudo(window.urlEncomendasList);
+            } else if (data.errors) {
+                alert("Erros no formulário: " + JSON.stringify(data.errors));
+            } else {
+                alert(data.message || 'Erro ao criar encomenda.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error);
+            alert('Erro inesperado.');
+        })
+});
+
+
+
+
+function deleteEncomenda(id, atleta, equipamento) {
+    if (confirm("Tem certeza que deseja apagar essa encomenda de " + equipamento + " para " + atleta + "?")) {
+        fetch('/equipamentos/encomenda_delete/' + id + '/')
+            .then(response => response.json())
+            .then(data => {
+                if (data.success || data.message) {
+                    alert(data.message)
+                    atletaSelectEncomendas()
+                }
+                else {
+                    alert(data.message || 'Erro ao apagar encomenda.');
+                }
+            })
+
+    }
+    else {
+        alert("Operação cancelada");
+    }
+}
+
+
+function alterarEstadoEncomenda(idEncomenda) {
+    let checkbox = event.target
+    if (confirm("Tem certeza que deseja alterar o estado dessa encomenda?")) {
+        fetch('/equipamentos/alterar_estado_encomenda/' + idEncomenda + '/')
+            .then(response => response.json())
+            .then(data => {
+                if (data.success || data.message) {
+                    alert(data.message)
+                    atletaSelectEncomendas()
+                }
+                else {
+                    alert(data.message || 'Erro ao apagar encomenda.');
+                }
+            })
+    }
+    else {
+        checkbox.checked = !checkbox.checked
+    }
+}
--- staticfiles/js/geral.js
+++ staticfiles/js/geral.js
@@ -2,3 +2,67 @@ function menu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('show');
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const tooggleBtn = document.getElementById("theme-toggle");
+    const body = document.body;
+
+    // verificar a preferencia guardada
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        body.classList.add("dark-mode");
+        tooggleBtn.textContent = "☀️ Claro" 
+    } else if (savedTheme === "light") {
+        body.classList.remove("dark-mode");
+        tooggleBtn.textContent = "🌙 Escuro"
+    } else {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        body.classList.add("dark-mode");
+        tooggleBtn.textContent = "☀️ Claro" 
+      }
+    }
+
+    tooggleBtn.addEventListener("click", function() {
+       
+      if(body.classList.contains("dark-mode")){
+            body.classList.remove("dark-mode");
+            tooggleBtn.textContent = "🌙 Escuro"
+            localStorage.setItem("theme", "light");
+            
+      } else {
+            body.classList.add("dark-mode");
+            tooggleBtn.textContent = "☀️ Claro"
+            localStorage.setItem("theme", "dark");
+      }
+        
+    });
+});
+
+
+window.addEventListener('popstate', function(event) {
+    this.location.reload();
+});
+
+
+$(document).on('click', '.btn-menu', function (e) {
+      e.preventDefault();
+      const url = $(this).attr('href')
+
+      $.get(url, function (data) {
+            $('#conteudo-dinamico').html(data)
+            window.history.pushState(null, null, url)
+       
+      })
+             
+});
+
+
+
+
+
+
+
+  
+
+
